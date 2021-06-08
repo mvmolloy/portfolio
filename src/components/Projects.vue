331b7faf396f09f52fd517468694abd0f7df221d
mvmolloy <mvamolloy@gmail.com>
2021-06-08 15:20:28 +0000

    add less project cards for smaller screens

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -27,6 +27,56 @@
                         </div>
                     </div>
                 </div>
+                <div v-for="project in projects.slice(0, 6)" :key="project.name" class="hidden md:block lg:hidden mb-10">
+                    <div class="w-full">
+                        <img :src="require('@/assets/img/gif/' + project.imgSrc)" alt="GIF animation of project" class="w-full my-5 mx-auto px-10 md:px-5">
+                    </div>
+                    <h2 class="subheader text-seafoam-400">{{project.name}}</h2>
+                    <div class="mt-5 text-xs lg:px-5">
+                    <p class="text-gray-400 tracking-widest text-xl sm:text-lg mb-1 mb-5 leading-relaxed font-thin">{{ project.description }}</p>
+                        <div class="2xl:px-10">
+                            <span v-for="tech in project.stack" :key="tech" class="border-gray-400 border text-gray-500 text-opacity-90 border-opacity-90 little-card rounded-full">
+                                {{tech}}
+                            </span>
+                            <div>
+                                <span v-if="project.building" class="border-gray-300 border text-gray-300 little-card rounded-full">
+                                    <i class="fas fa-laptop pr-2"></i> Demo
+                                </span>
+                                <a v-if="project.demo" :href="project.demo" target="_blank" class="link-btn little-card">
+                                    <i class="fas fa-laptop pr-2"></i> Demo
+                                </a>
+                                <a v-if="project.repo" :href="project.repo" target="_blank" class="link-btn little-card">
+                                    <i class="fas fa-code pr-2"></i> Code
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div v-for="project in projects.slice(0, 4)" :key="project.name" class="md:hidden mb-10">
+                    <div class="w-full">
+                        <img :src="require('@/assets/img/gif/' + project.imgSrc)" alt="GIF animation of project" class="w-full my-5 mx-auto px-10 md:px-5">
+                    </div>
+                    <h2 class="subheader text-seafoam-400">{{project.name}}</h2>
+                    <div class="mt-5 text-xs lg:px-5">
+                    <p class="text-gray-400 tracking-widest text-xl sm:text-lg mb-1 mb-5 leading-relaxed font-thin">{{ project.description }}</p>
+                        <div class="2xl:px-10">
+                            <span v-for="tech in project.stack" :key="tech" class="border-gray-400 border text-gray-500 text-opacity-90 border-opacity-90 little-card rounded-full">
+                                {{tech}}
+                            </span>
+                            <div>
+                                <span v-if="project.building" class="border-gray-300 border text-gray-300 little-card rounded-full">
+                                    <i class="fas fa-laptop pr-2"></i> Demo
+                                </span>
+                                <a v-if="project.demo" :href="project.demo" target="_blank" class="link-btn little-card">
+                                    <i class="fas fa-laptop pr-2"></i> Demo
+                                </a>
+                                <a v-if="project.repo" :href="project.repo" target="_blank" class="link-btn little-card">
+                                    <i class="fas fa-code pr-2"></i> Code
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
