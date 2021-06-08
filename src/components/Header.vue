6995e38cf8a3712f47213f366ff742ec507825d5
mvmolloy <mvamolloy@gmail.com>
2021-06-08 14:35:03 +0000

    update typed subheader text colour

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="home" class="skew">
         <div class="unskew absolute bottom-5 lg:bottom-14 inset-x-0 w-5/6 sm:w-1/2 lg:w-2/5 3xl:w-1/3 px-10 lg:px-20">
-            <h2 class="subheader text-green-100 tracking-wide 3xl:text-xl">I Build <span class="typed"></span></h2>
+            <h2 class="subheader text-greeamn-100 tracking-wide 3xl:text-xl">I Build <span class="typed"></span></h2>
             <h1 class="text-white tracking-widest text-xl sm:text-2xl 3xl:text-3xl mb-1 mb-5 leading-relaxed font-light">Hi, my name is Megan. I build software that helps keep the UK learning.</h1>
         </div>
     </section>
