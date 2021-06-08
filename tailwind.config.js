fcdad661d7cfb008f3ec1681151cb5aa41f1b336
mvmolloy <mvamolloy@gmail.com>
2021-06-08 13:57:36 +0000

    extend tailwind colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -5,6 +6,16 @@ module.exports = {
     extend: {
         height: {
             'vh80': '80vh',
+        },
+        colors: {
+            emerald: colors.emerald,
+            seafoam: {
+                50: '#f4f8f6',
+                100: '#d0e2d8',
+                300: '#6FA687',
+                400: '#639f7d',
+                500: '#5b9373'
+            },
         }
     },
   },
