0078dafecfb18037122451084b97ba4fd83491dd
mvmolloy <mvamolloy@gmail.com>
2021-06-09 12:10:49 +0000

    swich git main to git master

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:mvmolloy/portfolio.git main:gh-pages
+git push -f git@github.com:mvmolloy/portfolio.git master:gh-pages
 
 cd -
