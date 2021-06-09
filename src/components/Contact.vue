f243193c31b52f5290c9a59a3df62b253b2316e8
mvmolloy <mvamolloy@gmail.com>
2021-06-09 19:55:07 +0000

    fix -ve/+ve margins around geoglobe

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,12 @@
 <template>
-    <section id="contact" class="px-10 lg:px-20 pb-5 mt-10 lg:mt-0 bg-seafoam-50">
-     <div class="grid md:grid-cols-1">
-            <div class="mx-auto block lg:-mt-52 p-4">
-                <div id="geo-globe" class="pt-36 xs:pt-10">
+    <section id="contact" class="px-10 lg:px-20 pb-5 lg:mt-0 bg-seafoam-50">
+     <div class="grid md:grid-cols-1 lg:-mt-36">
+            <div class="mx-auto block -mt-40 xs:mt-0">
+                <div id="geo-globe" class="pt-36 xs:pt-10 lg:pt-0">
                     <canvas id="canvas"></canvas>
                 </div>
             </div>
-            <div class="text-center xs:mt-0">
+            <div class="text-center md:mt-10 lg:mt-5">
                 <div id="social-icons__wrapper" class="mb-4 text-3xl 3xl:text-4xl 2xl:mt-5">
                     <a href="https://www.linkedin.com/in/mvmolloy/" target="__blank" class="social-link"><i class="fab fa-linkedin"></i></a>
                     <a href="https://github.com/mvmolloy" target="__blank" class="social-link"><i class="fab fa-github-square"></i></a>
